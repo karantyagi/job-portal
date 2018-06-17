881935616d605def0cfa0a320fb3010df117dedf
karantyagi <tyagi.k@husky.neu.edu>
2018-06-17 16:56:57 -0400

    fixed server to deploy to heroku

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.use(express.static('./dist/job-portal'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname,'/dist/job-portal/index.html'));
+  res.sendFile(path.join('/dist/job-portal/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 4200);
