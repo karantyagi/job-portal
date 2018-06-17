1189d02682078a913c79f7277fb06aaac72e9121
karantyagi <tyagi.k@husky.neu.edu>
2018-06-17 17:05:29 -0400

    fixed server to deploy to heroku

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/job-portal'));
+app.use(express.static('./dist/cs5610-web-dev-project'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join('/dist/job-portal/index.html'));
+  res.sendFile(path.join('/dist/cs5610-web-dev-project/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
