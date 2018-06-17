92d913d52707a933e61ab572ecb9b129a8612cdc
karantyagi <tyagi.k@husky.neu.edu>
2018-06-17 16:40:49 -0400

    updated package.json to deploy to heroku

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static('./dist/your_app_name'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/index.html'));
+  res.sendFile(path.join(__dirname,'/dist/your_app_name/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
